4de3d7c4be3f0961fec9f178fe6b8adedc0d44a2
yayaT03 <dahianat45@outlook.es>
2024-03-22 17:02:08 -0500

    bovinos

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,12 +16,7 @@ app.use(express.json());
 app.use(cors({origin: "*"}));
 
 app.use("/bovinos ",require('./Routers/bovino.route'));
-//app.use("/detallepedido", require('./routes/detallepedido.route'));
-//app.use("/factura",require('./routes/factura.route'));
-//app.use("/ordenp",require('./routes/ordenp.route'));
-//app.use("/producto", require('./routes/producto.route'));
-//220
-//app.use("/usuario", require('./routes/usuario.route'));
+
 
 app.listen(app.get('Port'), () => { 
     console.log("servidor corriendo por el puerto", 
